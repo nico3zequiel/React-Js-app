8bde0ee7d8876ff77032ed26c68b08296aa3132a
unknown <jofrenicolas60@gmail.com>
2021-11-22 13:03:38 -0300

    NavBar.jsx created, adds a menu with different categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import './components/NavBar/styles.css' 
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return (
+        
+        
+        
+        <ul className= "navStyle">    
+
+            {/* LINK AL HOME */}        
+            <li className="home"><strong>React Store</strong></li> 
+
+            {/* CATEGORIAS */}
+            
+            <li className="list">Categoria 1</li> 
+            <li className="list">Categoria 2</li>
+            <li className="list">Categoria 3</li>
+            <li className="list">Categoria 4</li>
+        </ul>
+    )
+}   
+
+export default NavBar;
